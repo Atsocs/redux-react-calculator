c3239449e5f9b495f752222bc69005b8e9e43d71
Atsocs <atsocs.cc@gmail.com>
2021-10-29 04:29:31 -0300

    add some style

--- src/Calculator.js
+++ src/Calculator.js
@@ -10,56 +10,65 @@ class Calculator extends Component {
 
     const { stack, inputState } = this.props.calculatorState;
     return (
-      <div className="calculator">
-        <p className={"append"}>{stack[2] || 0}</p>
-        <p className={"append"}>{stack[1] || 0}</p>
-        <p className={inputState}>{stack[0] || 0}</p>
-        <div className="buttons-container">
-          <Button onClick={this.props.clearAction} color={red}>
-            C
-          </Button>
-          <Button onClick={this.props.operationAction} color={green}>
-            ^
-          </Button>
-          <Button onClick={this.props.operationAction} color={green}>
-            %
-          </Button>
-          <Button onClick={this.props.operationAction} color={green}>
-            &divide;
-          </Button>
-          <br />
-          <Button onClick={this.props.pressNumWithDispatch}>7</Button>
-          <Button onClick={this.props.pressNumWithDispatch}>8</Button>
-          <Button onClick={this.props.pressNumWithDispatch}>9</Button>
-          <Button onClick={this.props.operationAction} color={green}>
-            &times;
-          </Button>
-          <br />
-          <Button onClick={this.props.pressNumWithDispatch}>4</Button>
-          <Button onClick={this.props.pressNumWithDispatch}>5</Button>
-          <Button onClick={this.props.pressNumWithDispatch}>6</Button>
-          <Button onClick={this.props.operationAction} color={green}>
-            &minus;
-          </Button>
-          <br />
-          <Button onClick={this.props.pressNumWithDispatch}>1</Button>
-          <Button onClick={this.props.pressNumWithDispatch}>2</Button>
-          <Button onClick={this.props.pressNumWithDispatch}>3</Button>
-          <Button onClick={this.props.operationAction} color={green}>
-            +
-          </Button>
-          <br />
-          <Button onClick={this.props.oppositeAction}>&plusmn;</Button>
-          <Button onClick={this.props.pressNumWithDispatch}>0</Button>
-          <Button onClick={this.props.pressNumWithDispatch}>.</Button>
-          <Button
-            onClick={this.props.enterAction}
-            color={white}
-            bgcolor={green}
-          >
-            &#9166; {/* enter */}
-          </Button>
-        </div>{" "}
+      <div>
+        <h1>
+          The{" "}
+          <a href="https://en.wikipedia.org/wiki/Reverse_Polish_notation">
+            RPN
+          </a>{" "}
+          Calculator App
+        </h1>
+        <div className="calculator">
+          <p className={"append"}>{stack[2] || 0}</p>
+          <p className={"append"}>{stack[1] || 0}</p>
+          <p className={inputState}>{stack[0] || 0}</p>
+          <div className="buttons-container">
+            <Button onClick={this.props.clearAction} color={red}>
+              C
+            </Button>
+            <Button onClick={this.props.operationAction} color={green}>
+              ^
+            </Button>
+            <Button onClick={this.props.operationAction} color={green}>
+              %
+            </Button>
+            <Button onClick={this.props.operationAction} color={green}>
+              &divide;
+            </Button>
+            <br />
+            <Button onClick={this.props.pressNumWithDispatch}>7</Button>
+            <Button onClick={this.props.pressNumWithDispatch}>8</Button>
+            <Button onClick={this.props.pressNumWithDispatch}>9</Button>
+            <Button onClick={this.props.operationAction} color={green}>
+              &times;
+            </Button>
+            <br />
+            <Button onClick={this.props.pressNumWithDispatch}>4</Button>
+            <Button onClick={this.props.pressNumWithDispatch}>5</Button>
+            <Button onClick={this.props.pressNumWithDispatch}>6</Button>
+            <Button onClick={this.props.operationAction} color={green}>
+              &minus;
+            </Button>
+            <br />
+            <Button onClick={this.props.pressNumWithDispatch}>1</Button>
+            <Button onClick={this.props.pressNumWithDispatch}>2</Button>
+            <Button onClick={this.props.pressNumWithDispatch}>3</Button>
+            <Button onClick={this.props.operationAction} color={green}>
+              +
+            </Button>
+            <br />
+            <Button onClick={this.props.oppositeAction}>&plusmn;</Button>
+            <Button onClick={this.props.pressNumWithDispatch}>0</Button>
+            <Button onClick={this.props.pressNumWithDispatch}>.</Button>
+            <Button
+              onClick={this.props.enterAction}
+              color={white}
+              bgcolor={green}
+            >
+              &#9166; {/* enter */}
+            </Button>
+          </div>{" "}
+        </div>
       </div>
     );
   }
